709936a224ef13e9929d6a76573cdf6b3be43b25
konstantin-a-maslov <k.maslov@sheffield.ac.uk>
2026-09-18 12:21:54 +0100

    Implemented [and tested] ConvNeXtV2 backbone

--- model/convnextv2.py
+++ model/convnextv2.py
@@ -14,12 +14,16 @@ class GlobalResponseNormalisation(eqx.Module):
         self.beta = jnp.zeros((dim, 1, 1))
         self.eps = eps
 
-    def __call__(self, x, mask=1.0):
+    def __call__(self, x, mask=None):
+        if mask is None:
+            mask = jnp.ones_like(x[:1])
+
         gx = jnp.sqrt(
             jnp.sum(x * x * mask, axis=(1, 2), keepdims=True)
-            + self.eps
+            + self.eps # to avoid potential singularity
         )
         nx = gx / (gx.mean(axis=0, keepdims=True) + self.eps)
+
         return x + self.gamma * (x * nx) + self.beta
 
 
@@ -56,7 +60,10 @@ class ConvNeXtV2Block(eqx.Module):
             key=next(keys),
         )
 
-    def __call__(self, x, mask=1.0):
+    def __call__(self, x, mask=None):
+        if mask is None:
+            mask = jnp.ones_like(x[:1])
+
         x = x * mask
         y = self.conv(x) * mask
         y = utils.spatial_vmap(self.ln)(y)
@@ -64,23 +71,109 @@ class ConvNeXtV2Block(eqx.Module):
         y = jax.nn.gelu(y)
         y = self.grn(y, mask)
         y = self.proj2(y)
+
         return (x + y) * mask
 
 
-class ConvNeXtV2(eqx.Module):
-    blocks: tuple[ConvNeXtV2Block]
-    downs: tuple[eqx.nn.Conv2d]
+class DownsampleBlock(eqx.Module):
+    ln: eqx.nn.LayerNorm
+    conv: eqx.nn.Conv2d
+    mask_pool: eqx.nn.MaxPool2d
 
+    def __init__(self, in_channels, out_channels, key):
+        self.ln = eqx.nn.LayerNorm(shape=in_channels)
+        self.conv = eqx.nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=2,
+            stride=2,
+            key=key,
+        )
+        self.mask_pool = eqx.nn.MaxPool2d(
+            kernel_size=2,
+            stride=2,
+        )
+
+    def __call__(self, x, mask=None):
+        if mask is None:
+            mask = jnp.ones_like(x[:1])
+
+        y = utils.spatial_vmap(self.ln)(x)
+        y = self.conv(y)
+        mask = 1.0 - self.mask_pool(1.0 - mask)
+
+        return y, mask
+
+
+class ConvNeXtV2(eqx.Module):
+    stages: int
+    blocks: tuple[tuple[ConvNeXtV2Block, ...], ...]
+    downs: tuple[eqx.Module, ...]
+    
     def __init__(self, depths, dims, key):
         keys = utils.key_gen(key)
+        self.stages = len(depths)
 
-    def __call__(self, x, mask=1.0):
-        pass
+        blocks = []
+        downs = []
+
+        for stage in range(self.stages):
+            stage_blocks = tuple(
+                ConvNeXtV2Block(
+                    dim=dims[stage],
+                    key=next(keys),
+                )
+                for _ in range(depths[stage])
+            )
+            blocks.append(stage_blocks)
+
+            if stage < self.stages - 1:
+                down = DownsampleBlock(
+                    in_channels=dims[stage],
+                    out_channels=dims[stage + 1],
+                    key=next(keys),
+                )
+                downs.append(down)
+
+        self.blocks = tuple(blocks)
+        self.downs = tuple(downs)
+
+    def __call__(self, x, mask=None):
+        if mask is None:
+            mask = jnp.ones_like(x[:1])
+
+        y, outputs = x, []
+
+        for stage in range(self.stages):
+            for block in self.blocks[stage]:
+                y = block(y, mask)
+            outputs.append(y)
+
+            if stage < self.stages - 1:
+                y, mask = self.downs[stage](y, mask)
+
+        return outputs
+
+
+def ConvNeXtV2Atto(key):
+    return ConvNeXtV2(
+        depths=[2, 2, 6, 2], 
+        dims=[40, 80, 160, 320], 
+        key=key,
+    )
 
 
 def ConvNeXtV2Tiny(key):
     return ConvNeXtV2(
         depths=[3, 3, 9, 3], 
         dims=[96, 192, 384, 768], 
-        key=key
+        key=key,
+    )
+
+
+def ConvNeXtV2Base(key):
+    return ConvNeXtV2(
+        depths=[3, 3, 27, 3], 
+        dims=[128, 256, 512, 1024], 
+        key=key,
     )
